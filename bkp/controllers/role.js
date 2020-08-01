69a60a9af139be53965557cace78ea67617da1e3
Maty Ramirez <matiasramirez@medife.com.ar>
2020-08-01 00:53:59 -0300

    Deleted: Old code folder

--- bkp/controllers/role.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-var Role = require('../models/role')
-
-exports.getRoles = (req, res) => {
-    Role.find(null, (result) => {
-        res.status(result.code).send(result.description)
-    })
-}
--- bkp/services/aws.js
+++
@@ -1,8 +0,0 @@
-var aws = require('aws-sdk');
-
-aws.config.update({
-	accessKeyId:'AKIAIGQ6VUQJ5O32AV2A',
-	secretAccessKey:'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'
-});
-
-module.exports = aws;
